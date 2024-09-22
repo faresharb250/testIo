d6808ccc7aece8761354cef3ffeb3d25c53db41e
faresharb250 <faresharb580@gmail.com>
2024-09-22 14:56:33 +0300

    initial commit

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ app.use(cors());
 app.use("/", (req, res, next) => {
   res.status(200).json({ message: "Done" });
 });
+app.use("*", (req, res, next) => {
+  res.status(404).json({ message: "In-valid routing" });
+});
 
 connectDB()
 const server = app.listen(port, () =>
